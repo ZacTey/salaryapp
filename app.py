164dcde1caedeb1521128a7e306036c06757c814
Zac Tey <59616801+ZacTey@users.noreply.github.com>
2022-08-29 21:17:27 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
 from flask import Flask, render_template, request, redirect, jsonify
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 import simplejson
 import requests
 import json
@@ -9,7 +12,10 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
-
+@app.route("/search")
+def view_method():
+    dropdown_list = ['Air', 'Land', 'Sea']
+    return render_template('index.html', dropdown_list=dropdown_list)
 
 if __name__ == "__main__":
     app.run()
