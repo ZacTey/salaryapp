a51f8c8c74680b7255f57bb25cfa723b46bab090
Zac Tey <59616801+ZacTey@users.noreply.github.com>
2022-08-29 20:54:14 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -12,19 +12,17 @@ def index():
 
 @app.route('/', methods=['GET'])
 def dropdown():
-    jobs = ['job1', 'job2', 'job3']
-    return render_template('index.html', jobs=jobs)
+    colours = ['SBI', 'Kotak', 'Citi', 'AMEX', 'BOB', 'AXIS', 'HDFC', 'IDBI', 'YES', 'IndusInd']
+    return render_template('feba.html', colours=colours)
 
 
 @app.route('/dropdown', methods = ['POST'])
 def dropp():
-    dropdownval = request.form.get('job')
+    dropdownval = request.form.get('colour')
     print(dropdownval)
     return redirect("/", code=302)
 if __name__ == "__main__":
     app.run()
-
-
 
 if __name__ == "__main__":
     app.run(debug=False)
