bc3478414bf3795d3eccc8dbbbe68da3d040a945
Zac Tey <59616801+ZacTey@users.noreply.github.com>
2022-08-31 00:35:43 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -6,18 +6,31 @@ import simplejson
 import requests
 import json
 app = Flask(__name__)
-
+model = pickle.load(open('model.pkl', 'rb'))
 
 @app.route("/")
 def index():
     return render_template("index.html")
 
-def view_method():
-    dropdown_list = ['job1', 'job2', 'job3']
-    return render_template('index.html', dropdown_list=dropdown_list)
+@app.route('/predict',methods=['POST'])
+def predict():
+
+    int_features = [int(x) for x in request.form.values()]
+    final_features = [np.array(int_features)]
+    prediction = model.predict(final_features)
+
+    output = round(prediction[0], 2)
+
+    return render_template('index.html', prediction_text='Salary should be $ {}'.format(output))
+
+@app.route('/results',methods=['POST'])
+def results():
+
+    data = request.get_json(force=True)
+    prediction = model.predict([np.array(list(data.values()))])
+
+    output = prediction[0]
+    return jsonify(output)
 
 if __name__ == "__main__":
-    app.run()
-
-if __name__ == "__main__":
-    app.run(debug=False)
+    app.run(debug=True)
