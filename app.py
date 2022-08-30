130fd5104e86a1e01c85a58deb9c9b9eb4ad9075
Zac Tey <59616801+ZacTey@users.noreply.github.com>
2022-08-31 00:57:15 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ def predict():
 
     output = round(prediction[0], 2)
 
-    return render_template('index.html', prediction_text='Sales should be $ {}'.format(output))
+    return render_template('index.html', prediction_text='Salary should be $ {}'.format(output))
 
 @app.route('/results',methods=['POST'])
 def results():
