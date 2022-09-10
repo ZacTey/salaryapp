8b94558e0b6ab16f0f4d4fa4dd1393c75aad2165
Zac Tey <59616801+ZacTey@users.noreply.github.com>
2022-09-10 13:38:16 +0800

    Update app.py

--- app.py
+++ app.py
@@ -17,9 +17,9 @@ def predict():
     input4 = request.form.get('type')
     input5 = request.form.get('exp')
     int_features = list(input1) + list(input2) + list(input3) + list(input4) + list(input5)
-    final_features = [int_features]
+    final_features = np.array(int_features)
     prediction = model.predict(final_features)
-    output = prediction
+    output = prediction[0]
     return render_template('index.html', prediction_text='Salary should be $ {}'.format(final_features))
 
 
