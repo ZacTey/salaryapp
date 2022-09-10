934e15cdfa153f88b45986f66fb7520691df51d4
Zac Tey <59616801+ZacTey@users.noreply.github.com>
2022-09-10 10:13:26 +0800

    Update app.py

--- app.py
+++ app.py
@@ -12,18 +12,17 @@ def home():
 
 @app.route('/predict',methods=['POST'])
 def predict():
-
-	input1 = request.form['job_title']
-	input2 = request.form['location']
-	input3 = request.form['level']
-	input4 = request.form['type']
-	input5 = request.form['exp']
-	input6 = request.form['jobdes']
-	input_all = [input1] + [input2] + [input3] + [input4] + [input5] + [input6]
-	int_features = [input_all]
-	final_features = [np.array(int_features)]
-	prediction = model.predict(final_features)
-
+    
+    input1 = request.form['job_title']
+    input2 = request.form['location']
+    input3 = request.form['level']
+    input4 = request.form['type']
+    input5 = request.form['exp']
+    input6 = request.form['jobdes']
+    input_all = [input1] + [input2] + [input3] + [input4] + [input5] + [input6]
+    int_features = [input_all]
+    final_features = [np.array(int_features)]
+    prediction = model.predict(final_features)
 
     output = prediction[0]
     return render_template('index.html', prediction_text='Salary should be $ {}'.format(output))
