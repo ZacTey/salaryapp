d680abb717023a08860efbb9726b44063b06364f
Zac Tey <59616801+ZacTey@users.noreply.github.com>
2022-09-10 11:16:32 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -11,19 +11,17 @@ def home():
 
 @app.route('/predict',methods=['POST'])
 def predict():
-    if request.method == 'POST':
-        input1 = request.form.get("job_title")
-        input2 = request.form.get('location')
-        input3 = request.form.get('level')
-        input4 = request.form.get('type')
-        input5 = request.form.get('exp')
-        input6 = request.form.get('jobdes')
-        input_all = [input1] + [input2] + [input3] + [input4] + [input5]
-        int_features = [input_all]
-        final_features = [np.array(int_features)]
-        prediction = model.predict(final_features)
-        output = prediction[0]
-        return output
+    input1 = request.form.get("job_title")
+    input2 = request.form.get('location')
+    input3 = request.form.get('level')
+    input4 = request.form.get('type')
+    input5 = request.form.get('exp')
+    input6 = request.form.get('jobdes')
+    input_all = [input1] + [input2] + [input3] + [input4] + [input5]
+    int_features = [input_all]
+    final_features = [np.array(int_features)]
+    prediction = model.predict(final_features)
+    output = prediction[0]
     return render_template('index.html', prediction_text='Salary should be $ {}'.format(output))
 
 
