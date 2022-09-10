68af68943073c76e0065ff134c60c8192feea24b
Zac Tey <59616801+ZacTey@users.noreply.github.com>
2022-09-10 10:56:55 +0800

    Update app.py

--- app.py
+++ app.py
@@ -12,17 +12,18 @@ def home():
 @app.route('/predict',methods=['POST'])
 def predict():
     if request.method == 'POST':
-        input1 = request.form['job_title']
-        input2 = request.form['location']
-        input3 = request.form['level']
-        input4 = request.form['type']
-        input5 = request.form['exp']
-        input6 = request.form['jobdes']
+        input1 = request.form.get("job_title")
+        input2 = request.form.get('location')
+        input3 = request.form.get('level')
+        input4 = request.form.get('type')
+        input5 = request.form.get('exp')
+        input6 = request.form.get('jobdes')
         input_all = [input1] + [input2] + [input3] + [input4] + [input5]
         int_features = [input_all]
         final_features = [np.array(int_features)]
         prediction = model.predict(final_features)
         output = prediction[0]
+        return output
     return render_template('index.html', prediction_text='Salary should be $ {}'.format(output))
 
 @app.route('/results',methods=['POST'])
